a4b12b8e51a232f43e8299d1297a353626376cfd
crawford-j <crawford.sjusten@gmail.com>
2025-06-01 17:56:45 -0500

    Added functionality to buttons.

--- simple-calculator-tkinter/main.py
+++ simple-calculator-tkinter/main.py
@@ -2,7 +2,61 @@
 # This project is designed to help me deepen my understanding of Python programming and GUI development using the Tkinter library. 
 import tkinter as tk
 
-def handle_press():
+num1 = ""
+num2 = ""
+operator = ""
+
+def calculate():
+    global num1, num2, operator
+    num2 = float(lbl_display.cget("text"))
+    num1 = float(num1)
+    match operator:
+        case "+":
+            answer = num1 + num2
+        case "-":
+            answer = num1 - num2
+        case "x":
+            answer = num1 * num2
+        case "÷":
+            answer = num1 / num2
+    if float(answer).is_integer():
+        lbl_display.config(text = str(int(answer)))
+    else:
+        lbl_display.config(text = str(answer))
+    num1 = ""
+    num2 = ""
+    return
+
+def handle_press(key):
+    global num1, num2, operator
+    curr = lbl_display.cget("text")
+    # Number
+    if key.isnumeric():
+        lbl_display.config(text = curr + key)
+    # Clear
+    elif key == "C":
+        num1 = ""
+        num2 = ""
+        operator = ""
+        lbl_display.config(text = "")
+    # Decimal point
+    elif key == ".":
+        if "." in lbl_display.cget("text"):
+            return
+        else:
+            lbl_display.config(text = curr + key)
+    # Main operators
+    elif key in ["+", "-", "x", "÷"]:
+        if num1 == "":
+            operator = key
+            num1 = lbl_display.cget("text")
+            lbl_display.config(text = "")
+        else:
+            return
+    # Equals
+    elif key == "=":
+        calculate()
+
     return
 
 # Create the main window
@@ -30,7 +84,7 @@ for i in range(5):
 frm_operators.columnconfigure(0, weight = 1)
 
 # Create result box that displays numbers being entered
-lbl_display = tk.Label(frm_display, relief = tk.SUNKEN, text = "", anchor = "e")
+lbl_display = tk.Label(frm_display, relief = tk.SUNKEN, text = "", font = ('Arial', 12, 'bold'), anchor = "e")
 
 # Create and place number Buttons within number Frame
 num_labels = ["7", "8", "9",
