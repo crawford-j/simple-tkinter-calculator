33b6cd8858e4e3cef9d9292f508731d7fd518732
crawford-j <crawford.sjusten@gmail.com>
2025-06-01 19:07:07 -0500

    Added sign change, percent, and backspace buttons with functionality.

--- simple-calculator-tkinter/main.py
+++ simple-calculator-tkinter/main.py
@@ -6,6 +6,7 @@ num1 = ""
 num2 = ""
 operator = ""
 
+# Function to process calculations based on the operator chosen by the user
 def calculate():
     global num1, num2, operator
     num2 = float(lbl_display.cget("text"))
@@ -27,18 +28,15 @@ def calculate():
     num2 = ""
     return
 
+# Function to handle button presses
+# String label of button passed in as parameter 
 def handle_press(key):
-    global num1, num2, operator
+    global num1, num2, operator, curr
     curr = lbl_display.cget("text")
     # Number
     if key.isnumeric():
         lbl_display.config(text = curr + key)
-    # Clear
-    elif key == "C":
-        num1 = ""
-        num2 = ""
-        operator = ""
-        lbl_display.config(text = "")
+
     # Decimal point
     elif key == ".":
         if "." in lbl_display.cget("text"):
@@ -53,10 +51,33 @@ def handle_press(key):
             lbl_display.config(text = "")
         else:
             return
+    # Percent
+    elif key == "%":
+        if curr:
+            percent = float(curr) / 100
+            if percent.is_integer():
+                lbl_display.config(text = str(int(percent)))
+            else:
+                lbl_display.config(text = str(percent))
+    # Clear
+    elif key == "C":
+        num1 = ""
+        num2 = ""
+        operator = ""
+        lbl_display.config(text = "")
+    # Change Sign
+    elif key == "±":
+        opposite = float(curr) * (-1)
+        if opposite.is_integer():
+            lbl_display.config(text = int(opposite))
+        else:
+            lbl_display.config(text = opposite)
+    # Backspace
+    elif key == "⌫":
+        lbl_display.config(text = curr[:-1])
     # Equals
     elif key == "=":
         calculate()
-
     return
 
 # Create the main window
@@ -65,46 +86,35 @@ window.title("Simple Calculator")
 window.resizable(width = False, height = False)
 window.rowconfigure(0, weight = 1)
 window.rowconfigure(1, weight = 5)
-window.columnconfigure(0, weight = 3)
-window.columnconfigure(1, weight = 1)
+window.columnconfigure(0, weight = 1)
 window.minsize(300, 400)
 
 # Create frames for the calculator
 frm_display = tk.Frame(window)
 
-frm_nums = tk.Frame(window)
-for i in range(4):
-    frm_nums.rowconfigure(i, weight = 5)
-for j in range(3):
-    frm_nums.columnconfigure(j, weight = 4)
-
-frm_operators = tk.Frame(window)
+frm_buttons = tk.Frame(window)
 for i in range(5):
-    frm_operators.rowconfigure(i, weight = 4)
-frm_operators.columnconfigure(0, weight = 1)
+    frm_buttons.rowconfigure(i, weight = 1)
+for j in range(4):
+    frm_buttons.columnconfigure(j, weight = 1)
 
 # Create result box that displays numbers being entered
-lbl_display = tk.Label(frm_display, relief = tk.SUNKEN, text = "", font = ('Arial', 12, 'bold'), anchor = "e")
+lbl_display = tk.Label(frm_display, relief = tk.SUNKEN, text = "", font = ('Arial', 20, 'bold'), anchor = "e")
 
 # Create and place number Buttons within number Frame
-num_labels = ["7", "8", "9",
-              "4", "5", "6",
-              "1", "2", "3",
-              "C", "0", "."]
-for i, label in enumerate(num_labels):
-    btn = tk.Button(frm_nums, text = label, relief = tk.RAISED, font = ('Arial', 12, 'bold'))
+labels = ["%", "C", "⌫", "+",
+          "7", "8", "9", "-",
+          "4", "5", "6", "x",
+          "1", "2", "3", "÷",
+          "±", "0", ".", "="]
+for i, label in enumerate(labels):
+    if i != 19:
+        btn = tk.Button(frm_buttons, text = label, relief = tk.RAISED, font = ('Arial', 15, 'bold'))
+    else:
+        btn = tk.Button(frm_buttons, text = label, relief = tk.RAISED, font = ('Arial', 15, 'bold'), bg = "green")
     btn.config(command = lambda x = label: handle_press(x))
-    row = i // 3
-    col = i % 3
-    btn.grid(row = row, column = col, sticky = "nesw")
-
-# Create and place Buttons for operators within operator Frame
-operator_labels = ["+", "-", "x", "÷", "="]
-for i, label in enumerate(operator_labels):
-    btn = tk.Button(frm_operators, text = label, relief = tk.RAISED, font = ('Arial', 12, 'bold'))
-    btn.config(command = lambda x = label: handle_press(x))
-    row = i
-    col = 0
+    row = i // 4
+    col = i % 4
     btn.grid(row = row, column = col, sticky = "nesw")
 
 # Place display label in diplay frame
@@ -112,8 +122,6 @@ lbl_display.pack(fill = tk.BOTH, expand = True)
 
 # Place Frames within Window
 frm_display.grid(row = 0, column = 0, columnspan = 2, sticky = "nesw")
-frm_nums.grid(row = 1, column = 0, sticky = 'nesw')
-frm_operators.grid(row = 1, column = 1, sticky = "nesw")
+frm_buttons.grid(row = 1, column = 0, sticky = 'nesw')
 
 window.mainloop() 
-
